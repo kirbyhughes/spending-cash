1488bbe48d7a5384b3a0e8ea5447817d80a020fa
khughes <githubspam@89vx.net>
2022-10-23 11:00:01 -0700

    now put X - Y = Z in cash section

--- monkey.js
+++ monkey.js
@@ -9,18 +9,21 @@
 console.log ("Hi there!");
 setTimeout( function () {
 if(window.location.href.indexOf("page/login/app#/dashboard") > 0) {
-        let cash = document.querySelectorAll('[data-hj-masked=""].account__group-value.qa-account-group-value.tabular-numbers')[0].textContent;
-        let cards = document.querySelectorAll('[data-hj-masked=""].account__group-value.qa-account-group-value.tabular-numbers')[2].textContent;
-        cash = parseFloat(cash.replace(/[^\d.-]/g, ''));
-        cards = parseFloat(cards.replace(/[^\d.-]/g, ''));
-        const result = cash + cards;
+        let cashloc = document.querySelectorAll('[data-hj-masked=""].account__group-value.qa-account-group-value.tabular-numbers')[0];
+        let cashtext = cashloc.textContent;
+        let cardstext = document.querySelectorAll('[data-hj-masked=""].account__group-value.qa-account-group-value.tabular-numbers')[2].textContent;
+        cash = parseFloat(cashtext.replace(/[^\d.-]/g, ''));
+        cardstext = cardstext.replace('-', '');
+        cards = parseFloat(cardstext.replace(/[^\d.-]/g, ''));
+        const result = cash - cards;
         const formatter = new Intl.NumberFormat('en-US', {
                 style: 'currency',
                 currency: 'USD',
         });
 
         const currentA = document.querySelector(".menu__action.menu__action--referral-gift-box");
+        //cashloc.innerHTML = cashtext + "&nbsp &nbsp &nbsp &nbsp" + formatter.format(result);
+        cashloc.innerHTML = cashtext + " - " + cardstext + " = " + formatter.format(result);
         currentA.innerHTML = formatter.format(result);
-}
-}, 3000);
+}}, 3000);
 
